2d5a3aadecb6c762d439e18f486534d6c60c7488
Andrii Kohut <kogut.andriy@gmail.com>
2014-08-17 16:42:21 +0300

    Add food age

--- lib/fridge.rb
+++ lib/fridge.rb
@@ -1,5 +1,31 @@
 require_relative 'db'
 
+class Utils
+
+  def self.seconds_to_string(s)
+
+    # d = days, h = hours, m = minutes, s = seconds
+    m = (s / 60).floor
+    s = s % 60
+    h = (m / 60).floor
+    m = m % 60
+    d = (h / 24).floor
+    h = h % 24
+
+    output = "#{s} second#{Utils.pluralize(s)}" if (s > 0)
+    output = "#{m} minute#{Utils.pluralize(m)}, #{s} second#{Utils.pluralize(s)}" if (m > 0)
+    output = "#{h} hour#{Utils.pluralize(h)}, #{m} minute#{Utils.pluralize(m)}, #{s} second#{Utils.pluralize(s)}" if (h > 0)
+    output = "#{d} day#{Utils.pluralize(d)}, #{h} hour#{Utils.pluralize(h)}, #{m} minute#{Utils.pluralize(m)}, #{s} second#{Utils.pluralize(s)}" if (d > 0)
+
+    return output
+  end
+
+  def self.pluralize number 
+    return "s" unless number == 1
+    return ""
+  end
+
+end
 
 class MoldyFridge
   def initialize(db_path)
@@ -9,7 +35,10 @@ class MoldyFridge
 
   def list_food(order_by=:time_brought)
     @db.get_all_food(order_by).each do |food|
-      puts "#{food[:food_name]} - #{food[:time_brought].localtime}"
+      name = food[:food_name]
+      brought = food[:time_brought].localtime
+      age = Utils::seconds_to_string(Time.now - brought)
+      puts "#{name}\tBrought: #{brought}, age: #{age}"
     end
   end
 
